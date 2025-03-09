099dca6b058c2c1dfd07ef346b12c432efcb8db0
Azad Mammadov <45455149+Azad-Mammadov@users.noreply.github.com>
2025-03-09 11:24:48 +0100

    this is my new comment

--- FinancialCalculator.cpp
+++ FinancialCalculator.cpp
@@ -1,11 +1,45 @@
-//
-// Created by AzadMammadov on 04/03/2025.
-//
+#include <iostream>
 
+
+
+class monthlyPayment {
+  private:
+    int m_y{}; // number of years for loan
+    int n {m_y*12}; // total number of payments
+
+    double m_p{}; // loan amount
+    double m_r{}; //monthly interest rate
+
+  public:
+    double art{}; // annual interest rate
+
+
+
+  // - functions ------------------------------------- +
+  double monthlyPayment() {
+    double getMonthlyPayment{
+
+
+    };
+  }
+
+
+  double operator^(double base, int exp){
+    double result = 1;
+    for (int i = 1; i <= exp; i++) {
+      result *= base;
+    }
+    return result;
+    }
+};
 
 int main(){
-  int year;
+  GradeMap grades{};
 
+  grades["Joe"] = 'A';
+  grades["Frank"] = 'B';
 
+  std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
+  std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
   return 0;
 }
