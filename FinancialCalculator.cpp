f06748466b1ab463c92ed5560d23185231192622
Azad Mammadov <45455149+Azad-Mammadov@users.noreply.github.com>
2025-03-09 15:05:16 +0100

    Added function regarding Monthly payment

--- FinancialCalculator.cpp
+++ FinancialCalculator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <cmath>
 
-
+// Input: Loan amount, interest rate, time period
+// Output: Monthly payment, total interest, amortization schedule
 
 class monthlyPayment {
   private:
@@ -8,38 +10,40 @@ class monthlyPayment {
     int n {m_y*12}; // total number of payments
 
     double m_p{}; // loan amount
-    double m_r{}; //monthly interest rate
+    //double m_r{}; //monthly interest rate
+    double m_art{}; // annual interest rate
 
   public:
-    double art{}; // annual interest rate
 
+    monthlyPayment(double art, int p, int y): m_art{art}, m_p{p}, m_y{y} {}
 
 
   // - functions ------------------------------------- +
-  double monthlyPayment() {
-    double getMonthlyPayment{
+    double calcMonthlyPayment() {
 
-
-    };
+                             double const r = m_art/12*100; // monthly interest rate
+                             double numerator = m_p*r*std::pow(1+r,2);
+                             double denominator = std::pow(1+r,2)-1;
+                             return numerator/denominator;
   }
 
+  void printPayment () {
+    std::cout << "Your monthly payment by default is: " << calcMonthlyPayment() << std::endl;
+  }
 
-  double operator^(double base, int exp){
-    double result = 1;
-    for (int i = 1; i <= exp; i++) {
-      result *= base;
-    }
-    return result;
-    }
+  //  my overload ------------------------------------- +
+  // double operator^(double base, int exp){
+  //   double result = 1;
+  //   for (int i = 1; i <= exp; i++) {
+  //     result *= base;
+  //   }
+  //   return result;
+  //   }
 };
 
 int main(){
-  GradeMap grades{};
+   monthlyPayment myPayment(8, 100000, 10);
 
-  grades["Joe"] = 'A';
-  grades["Frank"] = 'B';
-
-  std::cout << "Joe has a grade of " << grades["Joe"] << '\n';
-  std::cout << "Frank has a grade of " << grades["Frank"] << '\n';
-  return 0;
+   myPayment.printPayment();
+   return 0;
 }
